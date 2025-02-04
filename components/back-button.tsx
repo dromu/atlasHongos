a920bfb3b9b896e93937133e2e6a138bbcd04c53
dromu <alejandromun5@gmail.com>
2025-02-04 13:07:08 -0500

    Version corregida retroce v4

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -1,19 +1,14 @@
-"use client"; // Asegúrate de agregar esto si es un componente cliente
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import { ArrowLeft } from "lucide-react";
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { ArrowLeft } from "lucide-react"
 
 export function BackButton() {
-  const router = useRouter();
-
   return (
-    <Button
-      variant="outline"
-      size="sm"
-      className="mb-4 bg-primary text-primary-foreground hover:bg-primary/90"
-      onClick={() => router.replace("/")} // Reemplaza la ruta actual sin guardar en el historial
-    >
-      <ArrowLeft className="mr-2 h-4 w-4" /> Regresar al Inicio
+    <Button asChild variant="outline" size="sm" className="mb-4 bg-primary text-primary-foreground hover:bg-primary/90">
+      <Link href="/">
+        <ArrowLeft className="mr-2 h-4 w-4" /> Regresar al Inicio
+      </Link>
     </Button>
-  );
+  )
 }
+
