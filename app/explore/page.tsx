a829b990610ba1b8a2704b462f00e6035c412c02
dromu <alejandromun5@gmail.com>
2025-02-04 08:56:08 -0500

    Version de tres columnas

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -9,7 +9,7 @@ export default async function ExplorePage() {
     <div className="container mx-auto py-8 px-4">
       <BackButton />
       <h1 className="text-3xl font-bold mb-8 text-primary">DESCRIPCIÓN DE LOS HONGOS DE INTERÉS BIOCULTURAL POR EL PUEBLO KOKONUKO DEL MUNICIPIO DE PURACÉ (2019-2022)</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols- gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols- gap-6">
         {fungi.map((fungus) => (
           <FungusCard key={fungus.id} fungus={fungus}  />
         ))}
