acc2bd085bebe462001ee806a0a1e94191a880ec
dromu <alejandromun5@gmail.com>
2025-02-04 12:50:48 -0500

    Version corregida retroce v3

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -1,17 +1,17 @@
-"use client";
+"use client"; // Asegúrate de agregar esto si es un componente cliente
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 
 export function BackButton() {
+  const router = useRouter();
+
   return (
     <Button
       variant="outline"
       size="sm"
       className="mb-4 bg-primary text-primary-foreground hover:bg-primary/90"
-      onClick={() => {
-        window.location.href = "/";
-        window.location.reload(); // Recarga la página para evitar cualquier caché de Next.js
-      }}
+      onClick={() => router.replace("/")} // Reemplaza la ruta actual sin guardar en el historial
     >
       <ArrowLeft className="mr-2 h-4 w-4" /> Regresar al Inicio
     </Button>
