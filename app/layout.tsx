67409180e38b30ca6d67bfd3b08ec431bf380668
dromu <alejandromun5@gmail.com>
2025-02-19 13:27:48 -0500

    Cambio de favicon y letras cursivas correcion

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,36 +19,25 @@ export const metadata: Metadata = {
 };
 
 
+
+
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      
+
       <head>
         <link rel="icon" href="/images/general/faviconFungus.ico" sizes="any" />
       </head>
-
-      <body>{children}</body>  
+      
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
-
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;  f
-// }>) {
-//   return (
-//     <html lang="en">
-//       <body
-//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-//       >
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
